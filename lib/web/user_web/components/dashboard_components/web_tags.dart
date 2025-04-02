b68ddafbb137ae74d2a9ba5105e53ba5dda9b1c6
Mike <mikebaliscao0@gmail.com>
2025-04-02 18:44:24 +0800

    commit	new file:   lib/web/user_web/components/dashboard_components/web_tags.dart commit	modified:   lib/web/user_web/pages/dashboard_web_page.dart

---
+++ lib/web/user_web/components/dashboard_components/web_tags.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+class WebTags extends StatefulWidget {
+  const WebTags({super.key});
+
+  @override
+  State<WebTags> createState() => _WebTagsState();
+}
+
+class _WebTagsState extends State<WebTags> {
+
+  int _selectedindex = 0;
+
+  final List <String> tags = [
+    "All",
+    "Nearby",
+    "Popular",
+    "Recommended"
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SizedBox(
+          height: 50,
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: tags.length,
+            itemBuilder: (context, index) {
+              return GestureDetector(
+                onTap: () {
+                  setState(() {
+                    _selectedindex = index;
+                  });
+                },
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 10),
+                  child: Column(
+                    children: [
+                      Text(
+                        tags[index],
+                        style: TextStyle(
+                          color: _selectedindex == index ? Colors.black : Colors.grey,
+                          fontWeight: _selectedindex == index ? FontWeight.bold : FontWeight.normal
+                        ),
+                      ),
+                      if (_selectedindex == index )
+                      Container(
+                        margin: const EdgeInsets.only(top: 4),
+                        height: 2,
+                        width: 20,
+                        color: Colors.black,
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
--- lib/web/user_web/pages/dashboard_web_page.dart
+++ lib/web/user_web/pages/dashboard_web_page.dart
@@ -1,5 +1,5 @@
-import 'package:capstone_app/mobile/user/components/dashboard_components/tags.dart';
 import 'package:capstone_app/web/user_web/components/dashboard_components/dashboard_tile_web.dart';
+import 'package:capstone_app/web/user_web/components/dashboard_components/web_tags.dart';
 import 'package:flutter/material.dart';
 
 class WebDashboardPage extends StatefulWidget {
@@ -13,9 +13,9 @@ class _WebDashboardPageState extends State<WebDashboardPage> {
   @override
   Widget build(BuildContext context) {
     return ListView(
-      padding: EdgeInsets.only(left: 60, right: 60),
+      padding: const EdgeInsets.only(left: 60, right: 60, top: 16),
       children: const [
-        MyTags(),
+        WebTags(),
         Padding(
           padding: EdgeInsets.all(16.0),
           child: SingleChildScrollView(
